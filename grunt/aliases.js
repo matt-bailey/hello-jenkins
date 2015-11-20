74331597d488cb7f09a9ba7889900711b8e99616
Matt Bailey <me@mattbailey.co>
2015-11-20 22:44:20 +0000

    Blah

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -46,15 +46,6 @@ module.exports = function(grunt, data) {
       tasks: [
         'concurrent:imgFirst'
       ]
-    },
-
-    report: {
-      description: 'Styles reporting',
-      tasks: [
-        'scsslint',
-        'csscss',
-        'stylestats'
-      ]
-    },
+    }
   };
 };
